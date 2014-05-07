1fb4fb658c64d0478d8a8c2bfedf09b40702c097
cixweb <diblasiogiovanni48@gmail.com>
2014-05-07 17:12:38 +0200

    notif

--- Project/workspace/MapYou/src/it/mapyou/persistence/Notification_DAO.java
+++ Project/workspace/MapYou/src/it/mapyou/persistence/Notification_DAO.java
@@ -9,11 +9,7 @@ import it.mapyou.model.Notification;
  * @author mapyou (mapyouu@gmail.com)
  *
  */
-<<<<<<< HEAD
-public interface Notification_DAO<Notification> extends DAOSubjectModelInterface<Notification> {
-=======
-public interface Notification_DAO extends
-		DAOSubjectModelInterface<Notification> {
->>>>>>> 93462540efb162e71945c2b40a28fbcae8d83a03
+public interface Notification_DAO extends DAOSubjectModelInterface<Notification> {
+
 
 }
