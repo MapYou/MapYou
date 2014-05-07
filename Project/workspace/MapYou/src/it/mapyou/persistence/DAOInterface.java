66f96b860eb17f73a47c028c36182d93913ceafd
g666 <giuseppe.fusco.666@gmail.com>
2014-05-07 16:14:34 +0200

    p

---
+++ Project/workspace/MapYou/src/it/mapyou/persistence/DAOInterface.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package it.mapyou.persistence;
+
+import java.util.List;
+
+/**
+ * @author mapyou (mapyouu@gmail.com)
+ *
+ */
+public interface DAOInterface<T> {
+
+	public boolean insert(T t);
+	
+	public boolean delete(T t);
+	
+	public boolean update(T t);
+	
+	public List<T> selectAll();
+}
