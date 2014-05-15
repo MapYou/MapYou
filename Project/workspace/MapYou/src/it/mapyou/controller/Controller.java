2b5e3c1a8be1c8d9efb499ef74eb72008527a3b3
g666 <giuseppe.fusco.666@gmail.com>
2014-05-15 09:06:00 +0200

    e

--- Project/workspace/MapYou/src/it/mapyou/controller/Controller.java
+++ Project/workspace/MapYou/src/it/mapyou/controller/Controller.java
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-/**
- * 
- */
-package it.mapyou.controller;
 
-import it.mapyou.model.User;
-
-/**
- * @author mapyou (mapyouu@gmail.com)
- *
- */
-public interface Controller {
-	
-	public boolean login(User user);
-	
-	public boolean registration(User user);
-	
-	public boolean forgotPassword(User user);
-	
-	public boolean disconnet();
-	
-	public void partecipate();
-
-}
-=======
 /**
  * 
  */
@@ -50,4 +25,3 @@ public interface Controller {
 	public void init();
 
 }
->>>>>>> 7232d1ffd4ea3a6209aeabb3fcdc9e6214100f78
