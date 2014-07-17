6f9d5b00b140feee9b70b30bf6e5edfcbe8ae037
cixweb <diblasiogiovanni48@gmail.com>
2014-07-17 11:22:53 +0200

    non

--- Project/workspace/MapYou/src/it/mapyou/view/Login.java
+++ Project/workspace/MapYou/src/it/mapyou/view/Login.java
@@ -51,19 +51,19 @@ public class Login extends FacebookController {
 		ed.putString("email", "peppe");
 		ed.commit();
 
-		Intent intent= new Intent(Login.this,DrawerMain.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		startActivity(intent);
-//		user=(EditText) findViewById(R.id.user_login_Login);
-//		password=(EditText) findViewById(R.id.user_password_Login);
-//		controller= new DeviceController();
-//		try {
-//			controller.init(getApplicationContext());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//
-//		logoutFacebookSession2();
+//		Intent intent= new Intent(Login.this,DrawerMain.class);
+//		intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+//		startActivity(intent);
+		user=(EditText) findViewById(R.id.user_login_Login);
+		password=(EditText) findViewById(R.id.user_password_Login);
+		controller= new DeviceController();
+		try {
+			controller.init(getApplicationContext());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		logoutFacebookSession2();
 	}
 
 	// onclick Login
