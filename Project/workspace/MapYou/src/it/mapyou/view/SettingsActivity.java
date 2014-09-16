9a4787602de9a9261e8782d45d4195710a6f13d8
g666 <giuseppe.fusco.666@gmail.com>
2014-09-16 17:25:31 +0200

    asda

--- Project/workspace/MapYou/src/it/mapyou/view/SettingsActivity.java
+++ Project/workspace/MapYou/src/it/mapyou/view/SettingsActivity.java
@@ -7,6 +7,7 @@ import it.mapyou.R;
 import it.mapyou.view.adapter.SettingsAdapter;
 import it.mapyou.view.settings.DeleteAccount;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -15,6 +16,16 @@ import android.widget.ListView;
  *
  */
 public class SettingsActivity extends Activity{
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onBackPressed()
+	 */
+	@Override
+	public void onBackPressed() {
+		Intent i = new Intent(this, DrawerMain.class);
+		i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+		startActivity(i);
+	}
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
--- Project/workspace/MapYou/src/it/mapyou/view/settings/DeleteAccount.java
+++ Project/workspace/MapYou/src/it/mapyou/view/settings/DeleteAccount.java
@@ -3,9 +3,19 @@
  */
 package it.mapyou.view.settings;
 
-import android.app.Activity;
+import it.mapyou.controller.DeviceController;
+import it.mapyou.network.SettingsServer;
 import it.mapyou.util.UtilAndroid;
+import it.mapyou.view.Login;
 import it.mapyou.view.Setting;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 
 /**
  * @author mapyou (mapyouu@gmail.com)
@@ -13,18 +23,57 @@ import it.mapyou.view.Setting;
  */
 public class DeleteAccount extends Setting{
 
+	private SharedPreferences sp;
 	/**
 	 * @param act
 	 * @param idView
 	 */
 	public DeleteAccount(Activity act, int idView) {
 		super(act, idView);
-		// TODO Auto-generated constructor stub
+		sp=PreferenceManager.getDefaultSharedPreferences(act);
 	}
 
 	@Override
 	public void performeSetting() {
-		UtilAndroid.makeToast(act, "This is a deleteAccount setting", 5000);
+		String userLogged=sp.getString(UtilAndroid.KEY_NICKNAME_USER_LOGGED, "");
+
+		if(userLogged!=""){
+			new DeleteAccountTask().execute(userLogged.toString());
+		}else
+			UtilAndroid.makeToast(act, "Please log-in", 5000);
+	}
+
+	class DeleteAccountTask extends AsyncTask<String, Void, String>{
+
+		private HashMap<String, String> parameters=new HashMap<String, String>();
+
+		@Override
+		protected String doInBackground(String... params) {
+
+			String s="";
+			parameters.put("admin", ""+params[0].toString());
+			s=DeviceController.getInstance().getServer().request(SettingsServer.DELETE_ACCOUNT, DeviceController.getInstance().getServer().setParameters(parameters));
+
+			return s;
+		}
+
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+
+			if(result.contains("success_without_mapme") || result.contains("total_success")){
+				UtilAndroid.makeToast(act, "Account deleted", 5000);
+				Intent i = new Intent(act, Login.class);
+				sp.edit().clear().commit();
+				act.startActivity(i);
+			}else{
+				UtilAndroid.makeToast(act, "Account not deleted...Please log in", 5000);
+				Intent i = new Intent(act, Login.class);
+				act.startActivity(i);
+			}
+		}
+
 	}
 
 }
