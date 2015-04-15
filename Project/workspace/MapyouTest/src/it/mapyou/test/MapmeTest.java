8093d575dff1ba98a1142f3ad81e6e3791560ea4
g666 <giuseppe.fusco.666@gmail.com>
2015-04-15 12:06:27 +0200

    test get_location

--- Project/workspace/MapyouTest/src/it/mapyou/test/MapmeTest.java
+++ Project/workspace/MapyouTest/src/it/mapyou/test/MapmeTest.java
@@ -3,12 +3,17 @@ package it.mapyou.test;
 import it.mapyou.controller.DeviceController;
 import it.mapyou.controller.network.Server;
 import it.mapyou.controller.network.SettingsServer;
+import it.mapyou.controller.parsing.external.ConfigurationGeocodingApi;
+import it.mapyou.controller.parsing.external.ParserDataFromGeocoding;
 import it.mapyou.model.MapMe;
 
 import java.util.HashMap;
 import java.util.List;
 
 import junit.framework.TestCase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MapmeTest extends TestCase {
@@ -21,15 +26,15 @@ public class MapmeTest extends TestCase {
 
 
 	protected void setUp() throws Exception {
-		
+
 		dev = DeviceController.getInstance();
 		parameters = new HashMap<String, String>();
 		dev.init();
 		ser = dev.getServer();
 		page = SettingsServer.NEW_MAPME;
 	}
-	
- 
+
+
 	public void test_new_mapme(){
 
 		parameters.clear();
@@ -43,11 +48,11 @@ public class MapmeTest extends TestCase {
 		parameters.put("sadd", "startLocation");
 		parameters.put("eadd", "endLocation");
 		res=ser.request(page, ser.setParameters(parameters));
-		 
+
 		assertEquals("-1",res);
 	}
-	
-	 
+
+
 	public void get_mapme(){
 		page = SettingsServer.YOUR_MAPME;
 		parameters.clear();
@@ -55,7 +60,7 @@ public class MapmeTest extends TestCase {
 		parameters.put("inclusion", "1");
 		try {
 			List<MapMe> map = 
-			dev.getParsingController().getMapmeParser().parseListFromJsonObject(ser.requestJson(page, ser.setParameters(parameters)));
+					dev.getParsingController().getMapmeParser().parseListFromJsonObject(ser.requestJson(page, ser.setParameters(parameters)));
 			boolean f = false;
 			for(int i=0; i<map.size(); i++){
 				if(map.get(i).getName().equals("peppinoMapmehjl"))
@@ -64,6 +69,29 @@ public class MapmeTest extends TestCase {
 			if(!f)
 				fail("non ho trovato la mapme");
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void test_get_location(){
+		String url= new ConfigurationGeocodingApi("napoli").getUrlFromApi();
+		List<HashMap<String, String>> listOfpoints= null;
+		ParserDataFromGeocoding parser= new ParserDataFromGeocoding();
+		String data= parser.retrieveData(url);
+		try {
+			listOfpoints= parser.parse(new JSONObject(data));
+//			List<HashMap<String, String>> allElements= new ArrayList<HashMap<String,String>>();
+			int size = listOfpoints.size();
+			assertTrue(size>=1);
+			for(HashMap<String, String> m: listOfpoints){
+//				String lat=m.get("lat");
+//				String lon=m.get("lng");
+				String address=m.get("formatted_address");
+				assertTrue(address.contains("napoli"));
+			}
+
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
